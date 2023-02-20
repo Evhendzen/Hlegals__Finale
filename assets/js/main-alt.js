3fb8b5ce30f94c708554c7b5be6c55a1287ab46c
Evhendzen <Evhendze@gmail.com>
2023-02-21 00:44:10 +0200

    i just do it

--- assets/js/main-alt.js
+++ assets/js/main-alt.js
@@ -1,12 +1,12 @@
 // управление меню
+// управление меню
 const menuBtn = document.querySelector('.navbar__burger');
 const menuList = document.querySelector('.navbar__list');
 const header = document.querySelector('header');
 const body = document.querySelector('body');
 
 
-
-menuBtn.addEventListener('click',(e) => {
+menuBtn.addEventListener('click', (e) => {
     menuBtn.classList.toggle('active');
     header.classList.toggle('active');
     body.classList.toggle('active');
@@ -19,8 +19,8 @@ menuBtn.addEventListener('click',(e) => {
 });
 
 // Горизонтальный слайдер. Считали DOM элементы
-const headersList = document.querySelectorAll('.slider-alt');
-const indicatorsList = document.querySelectorAll('.slider-alt__indicator-alt');
+const headersLis = document.querySelectorAll('.slider-alt');
+const indicatorsLis = document.querySelectorAll('.slider-alt__indicator-alt');
 
 let index = 0;
 let interval = 5000;
@@ -29,12 +29,13 @@ let heightMax = null;
 
 setInterval(() => {
     // снимаем классы active для первых элементов
-
+    headersLis[index].classList.toggle('active');
+    indicatorsLis[index].classList.toggle('active');
     // увеличиваем индекс, пока не превышено количество элементов
-    index = (index + 1) % headersList.length;
+    index = (index + 1) % headersLis.length;
     // ставим классы active следующим элементам
-    headersList[index].classList.toggle('-active');
-    indicatorsList[index].classList.toggle('-active');
+    headersLis[index].classList.toggle('active');
+    indicatorsLis[index].classList.toggle('active');
 }, interval);
 
 // вычисление и изменение высоты блока описания под максимальный текст
